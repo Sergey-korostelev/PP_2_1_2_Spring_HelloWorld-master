33b481f3bde81bae88d230c9b4493701494f2f7b
Korostelev Sergey <Coloredkor@inbox.ru>
2024-05-16 20:00:15 +0500

    Examination

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("catBean");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("catBean");
+        System.out.println(bean3 == bean4);
+
+
+    }
+}
